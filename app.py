9709245e3a76730a4c9162ab8645f66bc393a07a
PRADIP <74247546+PRADIPMAHAJAN@users.noreply.github.com>
2021-07-21 20:12:52 +0530

    secreatekey_and_mail_change

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,flash,redirect
 from form import RegistrationForm,LoginForm
 
 app=Flask(__name__,template_folder='template')
-app.config['SECRET_KEY']='9f04d9180921775caedad82753391e121a94eb526a3fa577313da5281d8bfdba'
+app.config['SECRET_KEY']=''
 
 @app.route('/about')
 def Deshboard():
@@ -17,9 +17,9 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'mahajanpradip031@gmail.com' and form.Password.data == 'password':
+        if form.email.data == 'mahajanpradip@gmail.com' and form.Password.data == 'password':
             flash('you have been logged in!','success')
-            return redirect('http://127.0.0.1:1880/ui/#!/0?socketid=SAdMD4RqAG7qpndUAAAA')
+            return redirect('http://127.0.0.1:1880/=SAdMD4RqAG7qpndUAAAA')
         else:
             flash('login unsuccessfull please check username and password','denger')
     return render_template('login.html',title='Login',form=form)
